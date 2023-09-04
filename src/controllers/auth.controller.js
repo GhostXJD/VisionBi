ba8111361a186794b6e6d3cfd379fcea3258fdd9
GhostXJD <82038257+GhostXJD@users.noreply.github.com>
2023-09-03 22:11:07 -0300

    Creacion token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,32 @@
 import usuario from "../models/usuario.model.js";
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from "../libs/jwt.js";
 
 export const registro = async (req, res) => {
     const { nombre, correo, password, active, tipoUsuario } = req.body;
     try {
+        const passwordHashs = await bcrypt.hash(password, 10)
+
         const newUsuario = new usuario({
             nombre,
             correo,
-            password,
+            password: passwordHashs,
             active,
             tipoUsuario
         });
 
-        await newUsuario.save();
-        console.log(newUsuario);
+        const userSaved = await newUsuario.save();
+        const token = await createAccessToken({ id: userSaved._id })
+        res.cookie('token', token);
+        res.json({
+            id: userSaved._id,
+            nombre: userSaved.nombre,
+            correo: userSaved.correo,
+            active: userSaved.active,
+            tipoUsuario: userSaved.tipoUsuario
+        });
     } catch (error) {
-        console.log(error);
+        res.status(500).json({ message: error.message });
     }
 
 }
--- src/db.js
+++ src/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        console.log("Esperando")
         await mongoose.connect("mongodb://127.0.0.1:27017/VisionBiDb");
         console.log("Connect to database")
     } catch (error) {
