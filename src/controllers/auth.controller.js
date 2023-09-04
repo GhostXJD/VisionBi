ac843e7cd7903c451ee9e02e0aea53af46a51624
newmoss <migu.badilla@duocuc.cl>
2023-09-03 22:54:33 -0300

    Login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -29,6 +29,38 @@ export const registro = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 
-}
+};
 
-export const login = (req, res) => res.send("login")
+export const login = async (req, res) => {
+    const { correo, password } = req.body;
+    try {
+
+        const usuarioFound = await usuario.findOne({ correo })
+
+        if (!usuarioFound) return res.status(400).json({message:"user not found"}); 
+
+        const isMatch = await bcrypt.compare(password, usuarioFound.password);
+
+        if (!isMatch) return res.status(400).json({message:"password incorrect"});
+
+        const token = await createAccessToken({ id: usuarioFound._id })
+        res.cookie('token', token);
+        res.json({
+            id: usuarioFound._id,
+            nombre: usuarioFound.nombre,
+            correo: usuarioFound.correo,
+            active: usuarioFound.active,
+            tipoUsuario: usuarioFound.tipoUsuario
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+};
+
+export const logout = async (req, res) => {
+    res.cookie('token', "",{
+        expires: new Date(0),
+    });
+    return res.sendStatus(200);
+}
