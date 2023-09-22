c2c8c13dab0c00bdb3ce2ce6185e665f10e48cab
GhostXJD <82038257+GhostXJD@users.noreply.github.com>
2023-09-22 17:25:54 -0300

    Rutas especificas por tipo de usuario

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -1,12 +1,22 @@
-import { Navigate,Outlet } from "react-router-dom"
-import { useAuth } from "./context/AuthContext"
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
 
-function ProtectedRoute() {
-    const { loading, isAuthenticated } = useAuth();
+function ProtectedRoute({ role, ...props }) {
+  const { loading, isAuthenticated, hasRole } = useAuth();
+  const location = useLocation();
 
-    if (loading) return <h1>Loading...</h1>;
-    if(!loading && !isAuthenticated) return <Navigate to="/login" replace />;
-    return <Outlet />;
+  if (loading) return <h1>Loading...</h1>;
+
+  if (!loading && !isAuthenticated) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+
+  if (role && !hasRole(role)) {
+    // El usuario no tiene el rol requerido para acceder a esta ruta
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet {...props} />;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -49,7 +49,7 @@ export const AuthProvider = ({ children }) => {
         setUsuario(null);
     }
 
-    const hasRole = (tipoUsuario) => usuario?.tipoUsuario === tipoUsuario;
+    const hasRole = (role) => usuario?.tipoUsuario === role;
 
     useEffect(() => {
         if (errors.length > 0) {
