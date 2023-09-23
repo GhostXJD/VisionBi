ca47ae8d5e7bf5c2530001a1f05b7702b71644b4
Estefani Navarro <82219313+steffieess@users.noreply.github.com>
2023-09-22 22:39:12 -0300

    Front Login
    Modificaciones en los input y warnings del front de Login Page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,15 +1,39 @@
+//Imports 
 import { useForm } from 'react-hook-form'
 import { useAuth } from '../context/AuthContext';
 import { Link } from 'react-router-dom';
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function LoginPage() {
-
+  //Constants
   const { register, handleSubmit, formState: { errors } } = useForm();
   const { signin, errors: signinErrors } = useAuth();
 
   const onSubmit = handleSubmit(data => {
     signin(data)
   })
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <div className='flex h-screen items-center justify-center'>
@@ -20,23 +44,59 @@ function LoginPage() {
           </div>
         ))
         }
-        <h1 className='text-2xl font-bold'></h1>
+        <div>
+        <h1 className='text-2xl font-bold'> Sign In to VisionBI</h1>
+        </div>
+
         <form
           onSubmit={onSubmit}>
-          <input type="email" {...register("correo", { required: true })}
-            className='w-full bg-zinc-700 text-white px4 py-2 rounded-md my-2'
-            placeholder=' Ingrese su correo'
-          />
-          {errors.correo && (
-            <p className='text-red-500'>Se necesita correo</p>
-          )}
-          <input type="password" {...register("password", { required: true })}
-            className='w-full bg-zinc-700 text-white px4 py-2 rounded-md my-2'
-            placeholder=' Ingrese su contraseña'
-          />
-          {errors.password && (
-            <p className='text-red-500'>Se necesita contraseña</p>
-          )}
+
+          <FormControl sx={{ m: 1, width: '100%' }} variant="outlined" className='w-full bg-zinc-700 text-white px4 py-2 rounded-md my-2'>
+              {/* Ingreso de Correo */}
+              <InputLabel htmlFor="outlined-adornment-password" text-white px4 py-2 rounded-md my-2 >Email</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={'mail'}
+                {...register("correo", { required: true })}
+                label="Password"
+              />
+              {/* Valido el ingreso de un Correo */}
+              {errors.correo && (
+                <Stack sx={{ width: '100%' }} spacing={0.5}>
+                  <Alert severity="error">Se requiere correo</Alert>
+                </Stack>
+              )}
+          </FormControl>
+          <FormControl sx={{ m: 1, width: '100%' }} variant="outlined" className='w-full bg-zinc-700 text-white px4 py-2 rounded-md my-2'>
+              {/* Ingreso de Contraseña */}
+              <InputLabel htmlFor="outlined-adornment-password" text-white px4 py-2 rounded-md my-2 >Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                {...register("password", { required: true })}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+
+              {/* Valido el ingreso de una contraseña */}
+              {errors.password && (
+                <Stack sx={{ width: '100%' }} spacing={0.5}>
+                  <Alert severity="error">Se requiere contraseña</Alert>
+                </Stack>
+              )}
+          </FormControl>
           <button className='bg-zinc-400 px-3 py-1 rounded-lg' type="submit">Ingresar</button>
         </form>
 
@@ -45,8 +105,7 @@ function LoginPage() {
             className='bg-sky-500 px-4 py-1 rounded-xl'>Crear una cuenta</Link>
         </p>
       </div>
-    </div>
+    </div >
   )
 }
-
 export default LoginPage
