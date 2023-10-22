89b84af42f4a89c18ef1966accacf52a5332fdee
newmoss <migu.badilla@duocuc.cl>
2023-10-22 12:11:33 -0300

    details

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,38 +7,39 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import { useLocation } from 'react-router-dom';
 import { Link } from "react-router-dom";
 
-export default function Footer({hiddenRoutes}){
-    const {pathname}=useLocation();
+export default function Footer({ hiddenRoutes }) {
+    const { pathname } = useLocation();
     if (hiddenRoutes.find((hiddenRoute) => pathname == hiddenRoute)) {
-        return 
+        return
     }
-return (
-    <div>
-        <section class="footer">
-            <div class="box-footer">
-                <div class="box">
-                    <h3>Contact</h3>
-                    <Link><a> <i><LocalPhoneRoundedIcon /></i> +56 2 2789 5411 </a></Link>
-                    <Link><a> <i><PhoneAndroidRoundedIcon /></i> +56 9 4245 6139 </a></Link>
-                    <Link to='/contactus'><a><i><EmailRoundedIcon /></i> Contact Us </a></Link>
-                </div>
+    return (
+        <div>
+            <section className="footer">
+                <div className="box-footer">
+                    <div className="box">
+                        <h3>Contact</h3>
+                        <Link><a> <i><LocalPhoneRoundedIcon /></i> +56 2 2789 5411 </a></Link>
+                        <Link><a> <i><PhoneAndroidRoundedIcon /></i> +56 9 4245 6139 </a></Link>
+                        <Link to='/contactus'><a><i><EmailRoundedIcon /></i> Contact Us </a></Link>
+                    </div>
 
-                <div class="box">
-                    <h3>About</h3>
-                    <Link to='/aboutus'><a><i></i>About us</a></Link>
-                    <Link to='/mission'><a><i></i>Mission</a></Link>
-                    <Link to='/vision'><a><i></i>Vision</a></Link>
-                </div>
+                    <div className="box">
+                        <h3>About</h3>
+                        <Link to='/aboutus'><a><i></i>About us</a></Link>
+                        <Link to='/mission'><a><i></i>Mission</a></Link>
+                        <Link to='/vision'><a><i></i>Vision</a></Link>
+                    </div>
 
-                <div class="box">
-                    <h3>Follow Us</h3>
-                    <Link><a><i><FacebookRoundedIcon /></i> VisionBI </a></Link>
-                    <Link><a><i><TwitterIcon /></i> @visionBI </a></Link>
-                    <Link><a><i><InstagramIcon /></i> @visionBI </a></Link>
+                    <div className="box">
+                        <h3>Follow Us</h3>
+                        <Link><a><i><FacebookRoundedIcon /></i> VisionBI </a></Link>
+                        <Link><a><i><TwitterIcon /></i> @visionBI </a></Link>
+                        <Link><a><i><InstagramIcon /></i> @visionBI </a></Link>
+                    </div>
                 </div>
-            </div>
-            <div class="credit"> &copy; created by <span>visionBI</span> | all right reserved </div>
-        </section>
-    </div>
-)
-};
+                <div className="credit"><p>&copy; 2023 <span>Vision BI</span>. All right reserved.</p>
+                </div>
+            </section>
+        </div>
+    )
+}
--- client/src/pages/Vision.jsx
+++ client/src/pages/Vision.jsx
@@ -8,11 +8,11 @@ function AboutUs() {
                     <div className="presentation">
                         <h3>VISION</h3>
                         <p>To be recognized as leaders in the field of data analytics and
-                     sales prediction. Our vision is to empower businesses worldwide,
-                      helping them reach their full potential, optimize their operations,
-                       and turn their data into competitive advantages. We aim to be the
-                       trusted partner for companies aspiring to a more successful and
-                        sustainable future.</p>
+                            sales prediction. Our vision is to empower businesses worldwide,
+                            helping them reach their full potential, optimize their operations,
+                            and turn their data into competitive advantages. We aim to be the
+                            trusted partner for companies aspiring to a more successful and
+                            sustainable future.</p>
                     </div>
                     <div className="presentation-photo">
                         <img src={vision} alt="vision" className='about-img' />
