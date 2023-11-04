aa1f8777d107ae5ffea47ae8adbb49a09394e70e
GhostXJD <82038257+GhostXJD@users.noreply.github.com>
2023-11-04 01:48:57 -0300

    Graficar prediccion por dia

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -65,6 +65,17 @@ export default function DashboardPage() {
     getPredict();
   }, []);
 
+  const calculateTotalPredictedSales = () => {
+    if (predictData.predictions && predictData.predictions.length > 0) {
+      const predictedSales = predictData.predictions.map((prediction) => prediction.skuValue);
+      const totalPredictedSales = predictedSales.reduce((total, prediction) => total + prediction, 0);
+      return totalPredictedSales;
+    }
+    return 0;
+  };
+  
+  const totalPredictedSales = calculateTotalPredictedSales();  
+
   const getCsv = async () => {
     try {
       const response = await getCsvDatoRequest(usuario.company);
@@ -93,7 +104,7 @@ export default function DashboardPage() {
   const getPredict = async () => {
     try {
       const res = await getPredictRequest(usuario.company);
-      //console.log('Predict: ', res.data)
+      console.log('Predict: ', res.data)
       setPredictData(res.data);
       setLoading(true)
     } catch (error) {
@@ -102,7 +113,7 @@ export default function DashboardPage() {
       setLoading(false)
     }
   };
-  
+
   const Prediction = () => {
     const sortedChartData = [["Date", "Total Value", "Predicted Value"]];
 
@@ -160,6 +171,12 @@ export default function DashboardPage() {
 
     for (const dateKey of sortedDates) {
       sortedChartData.push([dateKey, dailyTotal[dateKey].total, dailyTotal[dateKey].predicted]);
+    }
+
+    if (predictData.predictions && predictData.predictions.length > 0) {
+      predictData.predictions.forEach((prediction, index) => {
+        sortedChartData.push([prediction.date, null, prediction.skuValue]);
+      });
     }
 
     return (
@@ -234,6 +251,28 @@ export default function DashboardPage() {
                     </button>
                   </div>
                 </div>
+                <div className="bg-gray-100 dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-60 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <p className="font-bold text-gray-400">Predicted Sales</p>
+                      <p className="text-2xl">${totalPredictedSales}</p>
+                    </div>
+                    <button type="button" className="ml-auto text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl bg-amber-400 text-white">
+                      <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        strokeWidth="0"
+                        viewBox="0 0 16 16"
+                        height="1em"
+                        width="1em"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5.5 0 1 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+
               </div>
               {csvData.length > 0 && predictData.predictions ? (
                 <Prediction csvData={csvData} predictData={predictData} />
